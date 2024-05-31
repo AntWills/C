7f2148ce4924230723b0386c1d05768c73af1df1
Antonio Wills <antoniowillissilvasantos@gmail.com>
2024-05-31 20:54:20 -0300

    Criação da struct Queue

--- ED_Queue/src/Queue/Queue.h
+++ ED_Queue/src/Queue/Queue.h
@@ -1,4 +1,29 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+	int info;
+	struct Node* next;
+} Node;
+
+typedef struct Queue {
+	Node* root;
+	int size;
+
+	void (*enqueue)(struct Queue*, int info);
+	int (*dequeue)(struct Queue*);
+	int (*front)(struct Queue*);
+	bool (*isEmpty)(struct Queue*);
+	bool (*contains)(struct Queue*, int element);
+
+	void (*print)(struct Queue*);
+} Queue;
+
+Queue* newQueue();
+void freeQueue(Queue* queue);
+
 #endif
